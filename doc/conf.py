966dfe7cd3a436f8452e16c97328e720f882c484
MikeCharikov <mcharikov@ugcs.com>
2020-07-29 13:33:24 +0300

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -52,6 +52,14 @@ html_theme = 'pydata_sphinx_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 #html_static_path = ['_static']
+html_logo = 'images/logo.png'
+
+html_theme_options = {
+  "github_url": "https://github.com/ugcs/vsm-cpp-sdk",
+  "external_links": [
+    {"name": "UgCS", "url": "https://www.ugcs.com/"}
+   ]
+}
 
 
 # Specify the path to Doxyrest extensions for Sphinx:
