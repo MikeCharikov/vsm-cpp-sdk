b3e05a8c6045e2e376a06a3e8ccb765bf0e4d30b
MikeCharikov <mcharikov@ugcs.com>
2020-07-29 12:33:45 +0300

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -41,12 +41,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-extensions += ['sphinx_rtd_theme']
+#extensions += ['sphinx_rtd_theme']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata-sphinx-theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
